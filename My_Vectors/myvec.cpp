f4a54ce3c9b3b4cb8f58eecca24057ec4925e247
NourMellal <mellal.nour9@gmail.com>
2025-02-21 20:53:01 +0100

    Refactor Myvec class to use namespace and improve exception handling

--- My_Vectors/myvec.cpp
+++ My_Vectors/myvec.cpp
@@ -1,71 +1,122 @@
 #include <iostream>
 
-using namespace std;
 
-template <typename T>
-
-class Myvec
+namespace my_vec
 {
-private:
-    T* data;
-    size_t size;
-    size_t capacity;
+    using namespace std;
 
-    void resize()
-    {
-        size_t new_cap = capacity * 2;
-        T* new_data = new T[new_cap];
-        for (size_t i = 0; i < size; i++)
-            new_data[i] = data[i];
-        delete data;
-        data = new_data;
-        capacity = new_cap;
-    }
-public:
-    Myvec() : data(nullptr), size(0), capacity(1)
-    {
-        data = new T[capacity];
-    }
-    ~Myvec() {
-        delete data;
-    }
+    template <typename T>
 
-    void push_back(const T& value)
+    class Myvec
     {
-        if (size == capacity)
-            resize();
-        data[size++] = value;
-    }
+    private:
+        T* data;
+        size_t size;
+        size_t capacity;
 
-    size_t get_size() const {
-        return size;
-    }
-    size_t get_capacity() const {
-        return capacity;
-    }
-
-    void display()
-    {
-        for (size_t i = 0 ; i < size ; i++)
+        void resize()
         {
-            cout << data[i] << " ";
+            size_t new_cap = capacity * 2;
+            T* new_data = new T[new_cap];
+            for (size_t i = 0; i < size; i++)
+                new_data[i] = data[i];
+            delete data;
+            data = new_data;
+            capacity = new_cap;
         }
-    }
-    T& operator[](size_t index)
-    {
-        if (index >= size)
+    public:
+        Myvec() : data(nullptr), size(0), capacity(1)
         {
-            throw out_of_range("index out of range");
+            data = new T[capacity];
         }
-        return data[index];
+        ~Myvec() {
+            delete data;
+        }
+
+        void push_back(const T& value)
+        {
+            if (size == capacity)
+                resize();
+            data[size++] = value;
+        }
+
+        size_t get_size() const {
+            return size;
+        }
+        size_t get_capacity() const {
+            return capacity;
+        }
+
+        void display()
+        {
+            for (size_t i{} ; i < size ; i++)
+            {
+                cout << data[i] << " ";
+            }
+        }
+        T& operator[](size_t index)
+        {
+            try {
+                if (index >= size)
+                {
+                    throw std::out_of_range("index out of range");
+                }
+            } catch (const std::out_of_range &e) {
+                cout << "An exception thrown " << e.what() << endl;
+            }
+            return data[index];
+        }
+    };
+
+    int main(void) {
+        Myvec<int> vec;
+        vec.push_back(10);
+        vec.push_back(20);
+        vec.push_back(20);
+        vec.push_back(20);
+        vec.push_back(20);
+        vec.push_back(20);
+        vec.push_back(20);
+        vec.push_back(20);
+        vec.push_back(20);
+        vec.display();
+        cout << "\n";
+        cout << vec[3] << endl;
+        return 0;
     }
-};
+} // namespace my_vec
+
+
+namespace except
+{
+    using namespace std;
+
+    class A {};
+    class B {};
+
+    void unexpectedHandler()
+    {
+        cout << "unexpected catched\n";
+        throw A();
+    } 
+    void f() throw(A) {
+        throw B();
+    }
+
+    int main(void) {
+        set_unexpected(unexpectedHandler);
+        try {
+            
+        } catch(A&) {
+            cout << "from main\n";
+        }
+        qreturn 0;
+    }
+} // namespace except
+
+
 
 int main(void) {
-    Myvec<int> vec;
-    vec.push_back(10);
-    vec.push_back(20);
-    vec.display();
-    cout << "\n";
-    cout << vec.get_capacity() << endl;
+    // my_vec::main();
+    except::main();
 }
